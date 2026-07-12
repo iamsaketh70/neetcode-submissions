7cceecc5101573aeb8acb72ebe0fb930ab395837
Sakethram reddy Palugulla <iamsaketh01@gmail.com>
2026-07-12 17:27:59 -0400

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,36 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keystore={}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keystore:
+            self.keystore[key]=[]
+        self.keystore[key].append([value,timestamp])
+        
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.keystore:
+            return ""
+
+        values=self.keystore[key]
+        l,r=0,len(values)-1
+        res=""
+        
+        while l<=r:
+            mid=l+(r-l)//2
+
+            if values[mid][1]<=timestamp:
+                res=values[mid][0]
+                l=mid+1
+
+            else:
+                r=mid-1
+
+        return res
+
+            
+
+        
